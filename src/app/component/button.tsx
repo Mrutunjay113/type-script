5b30657503ebc0fbb01a80279742fad01b4637b5
Mrutunjay Yadav <42071203+Mrutunjay113@users.noreply.github.com>
2024-10-16 21:40:20 +0530

    Refactor button component: Add variable and function declarations and console log

--- src/app/component/button.tsx
+++ src/app/component/button.tsx
@@ -1,16 +1,16 @@
+//variable declaration
+
+const name: string = "John";
+const age: number = 25;
+console.log(name, age);
+
+//function declaration
+
+function convertCurrency(amount: number, currency: string): void {
+  console.log(`Converting ${amount} to ${currency}`);
+}
+convertCurrency(100, "USD");
 export default function Button() {
-  //variable declaration
-
-  const name: string = "John";
-  const age: number = 25;
-  console.log(name, age);
-
-  //function declaration
-
-  function convertCurrency(amount: number, currency: string): void {
-    console.log(`Converting ${amount} to ${currency}`);
-  }
-  convertCurrency(100, "USD");
   return (
     <button className="bg-blue-500 text-lg text-white rounded px-4 py-2 w-fit h-fit">
       Click me!
